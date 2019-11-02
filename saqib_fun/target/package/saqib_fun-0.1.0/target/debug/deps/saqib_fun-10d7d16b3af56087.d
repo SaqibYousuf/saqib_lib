2592bf581a9769dc9496e643573faf3a90078ba6
SaqibYousuf <saqibyousuf001@gmail.com>
2019-11-02 16:55:19 -0700

    my first rust library

---
+++ saqib_fun/target/package/saqib_fun-0.1.0/target/debug/deps/saqib_fun-10d7d16b3af56087.d
@@ -0,0 +1,7 @@
+C:\Users\kkc\Desktop\my_lib\saqib_lib\saqib_fun\target\package\saqib_fun-0.1.0\target\debug\deps\saqib_fun-10d7d16b3af56087.rmeta: src\lib.rs
+
+C:\Users\kkc\Desktop\my_lib\saqib_lib\saqib_fun\target\package\saqib_fun-0.1.0\target\debug\deps\libsaqib_fun-10d7d16b3af56087.rlib: src\lib.rs
+
+C:\Users\kkc\Desktop\my_lib\saqib_lib\saqib_fun\target\package\saqib_fun-0.1.0\target\debug\deps\saqib_fun-10d7d16b3af56087.d: src\lib.rs
+
+src\lib.rs:
